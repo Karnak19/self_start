41a91eacd014e11f52475d898f3a2a1aee1f1cff
fr51 <francoisemile5@orange.fr>
2015-03-17 14:10:19 +0100

    FE

--- classes/client.class.inc.php
+++ classes/client.class.inc.php
@@ -15,6 +15,21 @@
 
 		public function client ($id, $nom, $prenom, $regime, $classe, $lundi, $mardi, $mercredi, $jeudi, $vendredi, $solde)
 		{
-			$this->id_cli=$id_cli;
+			$this->id_cli=$id;
+			$this->nom_cli=$nom;
+			$this->prenom_cli=$prenom;
+			$this->reg_cli=$regime;
+			$this->clas_cli=$classe;
+			$this->lun=$lundi;
+			$this->mar=$mardi;
+			$this->mer=$mercredi;
+			$this->jeu=$jeudi;
+			$this->ven=$vendredi;
+			$this->solde_cli=$solde;
+		}
+
+		public function get_id_cli ()
+		{
+			return $this->id_cli;
 		}
 	}
