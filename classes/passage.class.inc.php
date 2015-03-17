81c7781a6ac47843c48575351c0c1550b8e4940e
fr51 <francoisemile5@orange.fr>
2015-03-17 14:37:18 +0100

    FE

--- classes/passage.class.inc.php
+++ classes/passage.class.inc.php
@@ -1,1 +1,23 @@
 <?php
+	class passage
+	{
+		private $id_passage=0;
+		private $date_passage=null;
+		private $serv_passage="";
+		private $lib_repas="";
+		private $mont_repas=0;
+
+		public function passage ($id, $date, $service, $libelle, $montant)
+		{
+			$this->id_passage=$id;
+			$this->date_passage=$date;
+			$this->serv_passage=$service;
+			$this->lib_repas=$libelle;
+			$this->mont_repas=$montant;
+		}
+
+		public function get_id ()
+		{
+			return $this->id_passage;
+		}
+	}
