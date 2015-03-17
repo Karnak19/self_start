8c7e06f53d252e509fa516ae681c8dce57930db4
fr51 <francoisemile5@orange.fr>
2015-03-17 14:03:20 +0100

    FE

--- classes/client.class.inc.php
+++ classes/client.class.inc.php
@@ -1,5 +1,15 @@
 <?php
 	class client
 	{
-		?
+		private $id_cli=0;
+		private $nom_cli="";
+		private $prenom_cli="";
+		private $reg_cli="";
+		private $clas_cli="";
+		private $lun=false;
+		private $mar=false;
+		private $mer=false;
+		private $jeu=false;
+		private $ven=false;
+		private $solde_cli=0;
 	}
