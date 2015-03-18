a4d234760ec73dacb6cace7489af10bed8c5d0d4
Karnak19 <basile64.v@gmail.com>
2015-03-18 11:48:30 +0100

    Accueil

--- accueil.php
+++ accueil.php
@@ -37,34 +37,6 @@
 				</div>
 			</center>
 		</div>
-<<<<<<< HEAD
-=======
-		<h1>
-			<?php
-				echo $_SESSION['type'];
-			?>
-		</h1>
-<<<<<<< HEAD
-
-		<a href="repas/repas.php"><img src="img/gear31.png">Repas</a>
-		<a href="passage/passage.php"><img src="img/shopping206.png">Passage</a>
-		<a href="compte/credit_compte.php"><img src="img/dollar41.png">Crédit compte</a>
-		<a href="fidelisation/fidelisation.php"><img src="img/calendar147.png">Fidélisation</a>
-		<a href="stats/stats.php"><img src="img/business24.png">Statistiques</a>
-
-=======
-		
-		<center>
-			<div class="menu">
-				<a href="repas/repas.php"><img src="img/gear31.png"></a>
-				<a href="passage/passage.php"><img src="img/shopping206.png"></a>
-				<a href="compte/credit_compte.php"><img src="img/dollar41.png"></a>
-				<a href="fidelisation/fidelisation.php"><img src="img/calendar147.png"></a>
-				<a href="stats/stats.php"><img src="img/business24.png"></a>
-			</div>
-		</center>
->>>>>>> 39b515d8d46fa12165f3f89289d07be4dfcd007b
->>>>>>> origin/master
 	</body>
 </html>
 <?php
