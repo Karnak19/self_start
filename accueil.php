e697684fbae695fdb734f2ba5e93a1cd743f25e2
Karnak19 <basile64.v@gmail.com>
2015-03-18 11:44:24 +0100

    Accueil + close + CSS
    Ajout de vérification de session sur l'accueil.
    Ajout de la page close pour se déconnecter.
    Modification du CSS en conséquence.

--- accueil.php
+++ accueil.php
@@ -1,26 +1,48 @@
+<?php
+	include 'inc/configure.inc.php';
+	
+	if(isset($_SESSION['type']))
+	{
+?>
 <html>
 	<?php
-		include ('inc/configure.inc.php');
+		//inclusion du head
 		include ('head.php');
 	?>
 	<body>
 		<div class="cornerlog">
-			<img src="img/logout13.png">
+			<a href="close.php"><img src="img/logout13.png"></a>
 		</div>
-		<h1>
-			<?php
-				echo $_SESSION['type'];
-			?>
-		</h1>
-		
-		<center>
-			<div class="menu">
-				<a href="repas/repas.php"><img src="img/gear31.png"></a>
-				<a href="passage/passage.php"><img src="img/shopping206.png"></a>
-				<a href="compte/credit_compte.php"><img src="img/dollar41.png"></a>
-				<a href="fidelisation/fidelisation.php"><img src="img/calendar147.png"></a>
-				<a href="stats/stats.php"><img src="img/business24.png"></a>
-			</div>
-		</center>
+		<div class="content">
+			<h1>
+				<?php
+					echo $_SESSION['type'];
+				?>
+			</h1>
+			
+			<center>
+				<div class="menu">
+					<?php
+						if($_SESSION['type'] == 'comptabilite' || $_SESSION['type'] == 'superadmin')
+			                        {	//Affichage des boutons pour la compta et l'admin
+					?>
+							<a href="repas/repas.php"><img src="img/gear31.png"></a>
+							<a href="compte/credit_compte.php"><img src="img/dollar41.png"></a>
+					<?php
+						}
+					?>
+					<a href="stats/stats.php"><img src="img/business24.png"></a>
+					<a href="passage/passage.php"><img src="img/shopping206.png"></a>
+					<a href="fidelisation/fidelisation.php"><img src="img/calendar147.png"></a>
+				</div>
+			</center>
+		</div>
 	</body>
 </html>
+<?php
+	}
+	else
+	{	//Si il n'y a pas de session, redirection vers l'index de connexion
+		header('location:index.php');
+	}
+?>
--- close.php
+++ close.php
@@ -1,14 +1,11 @@
 <?php
-mysql_connect("localhost", "root", "") or die(mysql_error());
-mysql_select_db("stage") or die(mysql_error());
+include 'inc/configure.inc.php';
 
-session_start();
-
-if($_POST['logout']=="Deconnexion")
-{
+/*if($_POST['logout']=="Deconnexion")
+{*/
     session_unset();
     session_destroy();
-}
+//}
 
 header("Location:index.php");
 
