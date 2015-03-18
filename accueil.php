f05f175ba7626397bee2cc23e30528fa368066fa
Stikyal <sageauplay@gmail.com>
2015-03-18 10:16:49 +0100

    test double modif

--- accueil.php
+++ accueil.php
@@ -9,18 +9,12 @@
 				echo $_SESSION['type'];
 			?>
 		</h1>
-<<<<<<< HEAD
-		test
 
-		
-			<i class="fa fa-money fa-5x"></i>
-
-=======
 		<a href="repas/repas.php"><img src="img/gear31.png">Repas</a>
 		<a href="passage/passage.php"><img src="img/shopping206.png">Passage</a>
 		<a href="compte/credit_compte.php"><img src="img/dollar41.png">Crédit compte</a>
 		<a href="fidelisation/fidelisation.php"><img src="img/calendar147.png">Fidélisation</a>
 		<a href="stats/stats.php"><img src="img/business24.png">Statistiques</a>
->>>>>>> origin/master
+
 	</body>
 </html>
