d32cf8b1b61cef0e94c0230e63920e8532c38dfd
Karnak19 <basile64.v@gmail.com>
2015-03-18 09:47:34 +0100

    logo

--- accueil.php
+++ accueil.php
@@ -9,10 +9,10 @@
 				echo $_SESSION['type'];
 			?>
 		</h1>
-		<a href="repas/repas.php">Repas</a>
-		<a href="passage/passage.php">Passage</a>
-		<a href="compte/credit_compte.php">Crédit compte</a>
-		<a href="fidelisation/fidelisation.php">Fidélisation</a>
-		<a href="stats/stats.php">Statistiques</a>
+		<a href="repas/repas.php"><img src="img/gear31.png">Repas</a>
+		<a href="passage/passage.php"><img src="img/shopping206.png">Passage</a>
+		<a href="compte/credit_compte.php"><img src="img/dollar41.png">Crédit compte</a>
+		<a href="fidelisation/fidelisation.php"><img src="img/calendar147.png">Fidélisation</a>
+		<a href="stats/stats.php"><img src="img/business24.png">Statistiques</a>
 	</body>
 </html>
