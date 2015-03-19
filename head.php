3d0d3cde83a2a5c3329c00eedb2e4498fb3ce30e
Karnak19 <basile64.v@gmail.com>
2015-03-19 11:33:10 +0100

    Sauvegarde de Jquery/JS/Botstrap

--- head.php
+++ head.php
@@ -1,26 +1,24 @@
 <head>
-		
-	<!-- include jQuery -->
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" type="text/javascript"></script>
-	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
-	<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
-		
-	<!-- Include Bootstrap -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-	<link href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-		
-	<!-- Include FontAwesome -->
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-         
-         <link rel="stylesheet" href="css/style.css">
+   <!-- include jQuery -->
+   <script src="js/jquery.min.js" type="text/javascript"></script>
+   <script src="js/jquery-ui.min.js" type="text/javascript"></script>
+   <link href="css/jquery-ui.css" rel="stylesheet">
+      
+   <!-- Include Bootstrap -->
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <script src="js/bootstrap.min.js"></script>
 
-		
-         <?php
-         echo $title;
-         
-         require_once('classes/client.class.inc.php');
-	?>
-	<!--<link rel="icon" type="image/x-icon" href="img/AAB.ico">-->
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+   <!-- Include FontAwesome -->
+   <link rel="stylesheet" href="css/font-awesome.min.css">
+
+   
+   <link rel="stylesheet" href="css/style.css">
+
+   <?php
+      echo $title;
+      
+      require_once('classes/client.class.inc.php');
+   ?>
+   <link rel="icon" type="image/x-icon" href="img/logo.png">
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
