556338c764305b97e96a96661cd58bf7f419604e
benji007 <benjidavidinfo@gmail.com>
2015-03-19 14:58:27 +0100

    ben

--- fidelisation/traitement_fidelisation.php
+++ fidelisation/traitement_fidelisation.php
@@ -1,6 +1,13 @@
 <?php
-	$client=new client();
-	switch ($fidelise)
+	require_once ("../classes/client.class.inc.php");
+	$id=$_GET ["id"];
+	$lundi=$_GET ["lundi"];
+	$mardi=$_GET ["mardi"];
+	$mercredi=$_GET ["mercredi"];
+	$jeudi=$_GET ["jeudi"];
+	$vendredi=$_GET ["vendredi"];
+	$client=new client ($id, null, null, null, null, null, null, null, null, null, null);
+	/*switch ($lundi, $mardi)
 	{
 		case "Lundi": $client->set_lundi ($id);
 		break;
@@ -12,5 +19,27 @@
 		break;
 		case "Vendredi": $client->set_vendredi ($id);
 		break;
+	}*/
+	if ($lundi=="1")
+	{
+		$client->set_lundi ($id);
 	}
+	if ($mardi=="1")
+	{
+		$client->set_mardi ($id);
+	}
+	if ($mercredi=="1")
+	{
+		$client->set_mercredi ($id);
+	}
+	if ($jeudi=="1")
+	{
+		$client->set_jeudi ($id);
+	}
+	if ($vendredi=="1")
+	{
+		$client->set_vendredi ($id);
+	}
+	
+	header ("Location: ../accueil.php");
 ?>
