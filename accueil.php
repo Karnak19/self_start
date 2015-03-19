7b644f0cd12c1ed7a0a8705e933403f319f610c7
Karnak19 <basile64.v@gmail.com>
2015-03-19 08:09:55 +0100

    Ajout images

--- accueil.php
+++ accueil.php
@@ -27,11 +27,11 @@
 			                        {	//Affichage des boutons pour la compta et l'admin
 					?>
 							<a href="repas/repas.php"><img src="img/gear31.png"></a>
-							<a href="compte/credit_compte.php"><img src="img/dollar41.png"></a>
+							<a href="compte/credit_compte.php"><img src="img/dollar159.png"></a>
 					<?php
 						}
 					?>
-					<a href="stats/stats.php"><img src="img/business24.png"></a>
+					<a href="stats/stats.php"><img src="img/statistics8.png"></a>
 					<a href="passage/passage.php"><img src="img/shopping206.png"></a>
 					<a href="fidelisation/fidelisation.php"><img src="img/calendar147.png"></a>
 				</div>
