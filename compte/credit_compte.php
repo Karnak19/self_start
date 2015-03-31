7570d22c28ac664d430091fd290bb977a5153287
benji007 <benjidavidinfo@gmail.com>
2015-03-31 11:51:28 +0200

    c'est qui le GG

--- compte/credit_compte.php
+++ compte/credit_compte.php
@@ -9,7 +9,7 @@
 
             <div class="modal-body">
 
-   <form method="post" action="traitement_credit.php">
+   <form method="post" action="compte/traitement_credit.php">
    <body>
 		<h1><title> Credit compte </title></h1>
        
@@ -17,14 +17,14 @@
 	
 	   <br>
 	   <label>Etudiant : </label>
-       <input type="text" name="etudiant" value="" /><br>
+       <input type="text" name="id_client" value="" /><br>
 	   
-       
        <label>Montant :</label>
        <input type="text" name="montant_cred" value="" /><br>
 		
 		<label> Date transaction : </label> 
 		<input type="date" name="date_cred" value="" /><br>
+		
 		<label> Type de paiement : </label> 
 		<select name="liste_type">
 		<option value=""> Choisissez un type de paiement </option>
--- compte/traitement_credit.php
+++ compte/traitement_credit.php
@@ -1,3 +1,13 @@
 <?php
-	if ($_POST['valider'])
-		
+	include ("../inc/configure.inc.php");
+	require_once ("../classes/credit_compte.class.inc.php");
+	
+	if (isset($_POST['valider']))
+	{
+		$client=$_POST['id_client'];
+		$type=$_POST['liste_type'];
+		$montant=$_POST['montant_cred'];
+		$date=date ("Y-m-d");
+		$credit= new lb_credit('', $date, $montant, $type);
+		$credit->crediter_compte($client, $type, $montant);
+	}	
