4ba7c2b4ad5a5f10478a77d213589924a2afbf9e
Karnak19 <basile64.v@gmail.com>
2015-03-31 14:31:52 +0200

    credit code + head

--- compte/credit_compte.php
+++ compte/credit_compte.php
@@ -1,39 +1,24 @@
-
 <div class="modal fade" id="cred" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-		<div class="modal-content">
-           <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                  <h4 class="modal-title" id="exampleModalLabel">Credit compte</h4>
-            </div>
-
-            <div class="modal-body">
-
-   <form method="post" action="compte/traitement_credit.php">
-   <body>
-		<h1><title> Credit compte </title></h1>
-       
-	   <br><h1 style= "text-align:center;">CREDIT COMPTE</h1><br><br>
-	
-	   <br>
-	   <label>Etudiant : </label>
-       <input type="text" name="id_client" value="" /><br>
-	   
-       <label>Montant :</label>
-       <input type="text" name="montant_cred" value="" /><br>
-		
-		<label> Date transaction : </label> 
-		<input type="date" name="date_cred" value="" /><br>
-		
-		<label> Type de paiement : </label> 
-		<select name="liste_type">
-		<option value=""> Choisissez un type de paiement </option>
-		<option value="1"> Especes </option>
-		<option value="2">Cheque</option>
-		</select><br><br>
-		
-		<input type="submit" name="valider" value="Valider">
-          </div>
-            </div>
-         </div>
-      </div>
+	<div class="modal-content">
+	    <div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		<h4 class="modal-title" id="exampleModalLabel">Credit compte</h4>
+	    </div>
+	    <div class="modal-body">
+		<form method="post" action="compte/traitement_credit.php">
+			<ul>Etudiant : <input type="text" name="id_client" value="" /></ul>
+			<ul>Montant : <input type="text" name="montant_cred" value="" /></ul>
+			<ul>Date transaction : <input type="date" name="date_cred" value="" /></ul>
+			<ul>Type de paiement : 
+			    <select name="ulste_type">
+				<option value="">Choisissez un type de paiement</option>
+				<option value="1">Especes</option>
+				<option value="2">Chèque</option>
+			    </select>
+			</ul>
+			<ul><input type="submit" name="valider" value="Valider"></ul> 
+	    </div>
+	</div>
+    </div>
+</div>
