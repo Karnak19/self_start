83d417853c2c160dadba31ef5ac4fc19192ec4e2
Stikyal <sageau3@gmail.com>
2015-03-31 08:48:07 +0200

    lien page credit passage boots

--- compte/credit_compte.php
+++ compte/credit_compte.php
@@ -1,8 +1,28 @@
-<html>
-   <?php
-      include ('../inc/configure.inc.php');
-      include ('../head.php');
-   ?>
-   <body>
-   </body>
-</html>
+     <div class="modal fade" id="cred" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+         <div class="modal-dialog">
+            <div class="modal-content">
+               <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                  <h4 class="modal-title" id="exampleModalLabel">Repas</h4>
+               </div>
+
+               <div class="modal-body">
+
+                <button id="btnAddd">New</button> 
+				<table id="tblData">	
+					<thead> 
+						<tr> 
+							<th>Name</th> 
+							<th>Phone</th> 
+							<th>Email</th> 
+							<th></th> 
+						</tr> 
+					</thead> 
+					<tbody>
+					
+					</tbody> 
+				</table>
+          </div>
+            </div>
+         </div>
+      </div>
