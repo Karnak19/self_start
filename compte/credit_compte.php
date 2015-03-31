f01eb4ebb0156d85bba3dba31240f85a9cb86b43
benji007 <benjidavidinfo@gmail.com>
2015-03-31 08:52:10 +0200

    Changement final

--- compte/credit_compte.php
+++ compte/credit_compte.php
@@ -34,33 +34,3 @@
 	   
    </body>
 </html>
-=======
-     <div class="modal fade" id="cred" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-         <div class="modal-dialog">
-            <div class="modal-content">
-               <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                  <h4 class="modal-title" id="exampleModalLabel">Repas</h4>
-               </div>
-
-               <div class="modal-body">
-
-                <button id="btnAddd">New</button> 
-				<table id="tblData">	
-					<thead> 
-						<tr> 
-							<th>Name</th> 
-							<th>Phone</th> 
-							<th>Email</th> 
-							<th></th> 
-						</tr> 
-					</thead> 
-					<tbody>
-					
-					</tbody> 
-				</table>
-          </div>
-            </div>
-         </div>
-      </div>
->>>>>>> origin/master
