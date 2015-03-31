2106347b503035ce70bd2ac51e02b2939c8ddf0a
benji007 <benjidavidinfo@gmail.com>
2015-03-31 08:50:23 +0200

    benj

--- compte/credit_compte.php
+++ compte/credit_compte.php
@@ -4,5 +4,32 @@
       include ('../head.php');
    ?>
    <body>
+		<h1><title> Credit compte </title></h1>
+       
+	   <br><h1 style= "text-align:center;">CREDIT COMPTE</h1><br><br>
+	
+	   <br>
+	   <label>Nom entreprise :</label>
+       <input type="text" name="NOM_ENT" value="" /><br>
+	   
+       
+       <label>Raison social :</label>
+       <input type="text" name="RAS_ENT" value="" /><br>
+		
+		<label> Rue : </label> 
+		<input type="text" name="RUE_ENT" value="" /><br>
+		<label> Code postal : </label> 
+		<input type="text" name="COP_ENT" value="" /><br>
+		<label> Ville : </label> 
+		<input type="text" name="VIL_ENT" value="" /><br>
+			
+		<label> Telephone : </label> 
+		<input type="text" name="TEL_ENT" value="" /><br>
+		<label> Mail : </label> 
+		<input type="text" name="MAI_ENT" value="" /><br>
+		<label> Login : </label> 
+		<input type="text" name="LOG_ENT" value=""required/><br>	
+	   
+	   
    </body>
 </html>
