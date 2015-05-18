e2c45f529ead92857cb3fc3788bc2313af5167b1
Karnak19 <basile64.v@gmail.com>
2015-05-18 17:22:29 +0200

    configure.inc
    Création d'une base en ligne afin de n'en avoir qu'une seule !!

--- inc/configure.inc.php
+++ inc/configure.inc.php
@@ -1,9 +1,9 @@
 <?php
 	session_start();
-	$hostname="127.0.0.1";
-	$user="root";
-	$pass="";
-	$db="lb_self";
+	$hostname="mysql.hostinger.fr";
+	$user="u481883468_root";
+	$pass="celxrdnoza";
+	$db="u481883468_self";
 	$database=@mysql_connect ("$hostname", "$user", "$pass");
 	@mysql_select_db ("$db", $database);
 	
