28ac721191529763d71aa17dc2026109414b6888
Karnak19 <basile64.v@gmail.com>
2018-10-04 15:35:40 +0200

    comment the session start

--- inc/configure.inc.php
+++ inc/configure.inc.php
@@ -1,14 +1,10 @@
 <?php
-	session_start();
+	/*session_start();
 	$hostname="127.0.0.1";
-	$user="root";
-	$pass="";
+	$user="BazESO";
+	$pass="root";
 	$db="lb_self";
 	
-	/*$hostname="mysql.hostinger.fr";
-	$user="u481883468_root";
-	$pass="celxrdnoza";
-	$db="u481883468_self";*/
 	
 	$database=@mysql_connect ("$hostname", "$user", "$pass");
 	@mysql_select_db ("$db", $database);
